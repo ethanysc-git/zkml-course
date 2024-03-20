18d5486b0ea62ad30c8beca5b14cb08a603265e7
Ethan Chen <acuratnam@gmail.com>
2024-03-20 23:59:50 +0800

    hardhat deploy update

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,4 +1,4 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional 
+// We require the Hardhat Runtime Environment explicitly here. This is optional
 // but useful for running the script in a standalone fashion through `node <script>`.
 //
 // When running the script with `hardhat run <script>` you'll find the Hardhat
@@ -8,15 +8,11 @@ const hre = require("hardhat");
 async function main() {
   const [deployer] = await hre.ethers.getSigners();
 
-  console.log(
-    "Deploying contracts with the account:",
-    deployer.address
-  );
-  
+  console.log("Deploying contracts with the account:", deployer.address);
+
   const Verifier = await hre.ethers.getContractFactory("Verifier");
   const verifier = await Verifier.deploy();
 
-  
   await verifier.deployed();
 
   console.log("Verifier deployed to:", verifier.address);
@@ -26,7 +22,33 @@ async function main() {
 // and properly handle errors.
 main()
   .then(() => process.exit(0))
-  .catch(error => {
+  .catch((error) => {
     console.error(error);
     process.exit(1);
   });
+
+// const { network } = require("hardhat")
+// const { developmentChains } = require("../helper-hardhat-config")
+// const { verify } = require("../utils/verify")
+
+// module.exports = async ({ getNamedAccounts, deployments }) => {
+//     const { deploy, log } = deployments
+//     const { deployer } = await getNamedAccounts()
+
+//     log("----------------------------------------------------")
+//     const arguments = []
+//     const basicNft = await deploy("BasicNft", {
+//         from: deployer,
+//         args: arguments,
+//         log: true,
+//         waitConfirmations: network.config.blockConfirmations || 1,
+//     })
+
+//     // Verify the deployment
+//     if (!developmentChains.includes(network.name) && process.env.ETHERSCAN_API_KEY) {
+//         log("Verifying...")
+//         await verify(basicNft.address, arguments)
+//     }
+// }
+
+// module.exports.tags = ["all", "basicnft", "main"]
